7927ca046a907466d39da14744078f58e6d155b1
Hadley Wickham <hadley@rstudio.com>
2018-12-05 22:40:03 +0000

    version 0.3.3

--- R/output.r
+++ R/output.r
@@ -15,11 +15,14 @@
 #' plot(nesting_prof)
 #' plot(reshape_prof)
 plot.profr <- function(x, ..., minlabel = 0.1, angle = 0) {
-  plot(1,1, xlim=range(x$start, x$end), ylim=range(x$level)+c(-0.5, 0.5), type="n", ..., xlab="time", ylab="level")
-  rect(x$start, x$level - 0.5, x$end, x$level +0.5, ...)
+  graphics::plot(1,1, xlim=range(x$start, x$end), ylim=range(x$level)+c(-0.5, 0.5), type="n", ..., xlab="time", ylab="level")
+  graphics::rect(x$start, x$level - 0.5, x$end, x$level +0.5, ...)
+
+  time <- NULL # quiet R CMD check note
   labels <- subset(x, time > max(time) * minlabel)
+
   if (nrow(labels) > 0)
-    text(labels$start, labels$level, labels$f, pos=4, srt=angle, ...)
+    graphics::text(labels$start, labels$level, labels$f, pos=4, srt=angle, ...)
 }
 
 #' Visualise profiling data with ggplot2.
@@ -34,25 +37,37 @@ plot.profr <- function(x, ..., minlabel = 0.1, angle = 0) {
 #' @param angle function label angle
 #' @seealso \code{\link{plot.profr}}
 #' @keywords hplot
-#' @export ggplot.profr
+#' @rawNamespace
+#' if (getRversion() >= "3.6.0") {
+#'   S3method(ggplot2::ggplot, "profr", ggplot.profr)
+#' } else {
+#'   export(ggplot.profr)
+#' }
 #' @examples
 #' if (require("ggplot2")) {
 #'   ggplot(nesting_prof)
 #'   ggplot(reshape_prof)
 #' }
 ggplot.profr <- function(data, ..., minlabel = 0.1, angle=0) {
-  if (!require("ggplot2", quietly = TRUE))
+  if (!requireNamespace("ggplot2", quietly = TRUE))
     stop("Please install ggplot2 to use this plotting method")
   data$range <- diff(range(data$time))
 
-  ggplot(as.data.frame(data)) +
-    geom_rect(aes(xmin = start, xmax = end, ymin = level - 0.5, ymax = level + 0.5),
+  # quiet R CMD check note
+  start <- NULL
+  end <- NULL
+  time <- NULL
+
+  ggplot2::ggplot(as.data.frame(data)) +
+    ggplot2::geom_rect(
+      ggplot2::aes(xmin = start, xmax = end, ymin = level - 0.5, ymax = level + 0.5),
       fill = "grey95", colour = "black", size = 0.5) +
-    geom_text(aes(start + range / 60, level, label = f),
+    ggplot2::geom_text(
+      ggplot2::aes(start + range / 60, level, label = f),
       data = subset(data, time > max(time) * minlabel),
       size = 4, angle = angle, hjust = 0) +
-    scale_y_continuous("time") +
-    scale_x_continuous("level")
+    ggplot2::scale_y_continuous("time") +
+    ggplot2::scale_x_continuous("level")
 }
 
 globalVariables(c("f", "level", "leaf"))
